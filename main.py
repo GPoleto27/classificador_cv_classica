430bc887ef61f1571b8f54cc682a7fbdd6058c9c
Guilherme Poleto <gpoleto@alunos.utfpr.edu.br>
2020-12-21 23:00:42 -0300

    minor fix

--- main.py
+++ main.py
@@ -37,65 +37,66 @@ while True:
     # Lê um frame
     success, img = cap.read()
 
-    # Desenha um retângulo branco na região de interesse
-    cv2.rectangle(img, (cx, cy), (cx + cw, cy + ch), (255, 255, 255), 2)
+    if success:
+        # Desenha um retângulo branco na região de interesse
+        cv2.rectangle(img, (cx, cy), (cx + cw, cy + ch), (255, 255, 255), 2)
 
-    cropped = img[cy: cy + ch, cx: cx + cw]
+        cropped = img[cy: cy + ch, cx: cx + cw]
 
-    # Aplica um delay
-    delay = float(1 / args.fps)
-    sleep(delay)
+        # Aplica um delay
+        delay = float(1 / args.fps)
+        sleep(delay)
 
-    # Pré-processa a imagem
-    # Converte para escala de cinza
-    gray = cv2.cvtColor(cropped, cv2.COLOR_BGR2GRAY)
-    # Aplica um desfoque gaussiano para suavização
-    blur = cv2.GaussianBlur(gray, (3, 3), 5)
-    # Aplica sutração de fundo
-    img_sub = subtraction.apply(blur)
-    # Dilata a imagem para aplicação de um filtro morfológico
-    dilat = cv2.dilate(img_sub, np.ones((5, 5)))
-    # Pega a estrutura morfológica da imagem
-    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
-    # Aplica operações morfológicas
-    # Mais detalhes em: https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_imgproc/py_morphological_ops/py_morphological_ops.html
-    dilated = cv2.morphologyEx(dilat, cv2.MORPH_CLOSE, kernel)
-    dilated = cv2.morphologyEx(dilated, cv2.MORPH_CLOSE, kernel)
+        # Pré-processa a imagem
+        # Converte para escala de cinza
+        gray = cv2.cvtColor(cropped, cv2.COLOR_BGR2GRAY)
+        # Aplica um desfoque gaussiano para suavização
+        blur = cv2.GaussianBlur(gray, (3, 3), 5)
+        # Aplica sutração de fundo
+        img_sub = subtraction.apply(blur)
+        # Dilata a imagem para aplicação de um filtro morfológico
+        dilat = cv2.dilate(img_sub, np.ones((5, 5)))
+        # Pega a estrutura morfológica da imagem
+        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
+        # Aplica operações morfológicas
+        # Mais detalhes em: https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_imgproc/py_morphological_ops/py_morphological_ops.html
+        dilated = cv2.morphologyEx(dilat, cv2.MORPH_CLOSE, kernel)
+        dilated = cv2.morphologyEx(dilated, cv2.MORPH_CLOSE, kernel)
 
-    contours, image = cv2.findContours(
-        dilated, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        contours, image = cv2.findContours(
+            dilated, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-    is_car = False
+        is_car = False
 
-    for (i, c) in enumerate(contours):
-        # Cria caixas limitantes
-        (x, y, w, h) = cv2.boundingRect(c)
+        for (i, c) in enumerate(contours):
+            # Cria caixas limitantes
+            (x, y, w, h) = cv2.boundingRect(c)
 
-        # Valida o contorno
-        validate_contour = (w >= args.min_width and h >= args.min_height)
-        if not validate_contour:
-            continue
+            # Valida o contorno
+            validate_contour = (w >= args.min_width and h >= args.min_height)
+            if not validate_contour:
+                continue
 
-        is_car = True
+            is_car = True
 
-        center = get_center(cx + x, cy + y, w, h)
+            center = get_center(cx + x, cy + y, w, h)
 
-        # Desenha um retângulo verde na região de interesse
-        cv2.rectangle(img, (cx, cy), (cx + cw, cy + ch), (0, 255, 0), 2)
+            # Desenha um retângulo verde na região de interesse
+            cv2.rectangle(img, (cx, cy), (cx + cw, cy + ch), (0, 255, 0), 2)
 
-        # Desenha um retângulo azul com um ponto vermelho na região de detecção
-        cv2.rectangle(img, (cx + x, cy + y),
-                      (cx + x + w, cy + y + h), (255, 0, 0), 2)
-        cv2.circle(img, center, 4, (0, 0, 255), -1)
+            # Desenha um retângulo azul com um ponto vermelho na região de detecção
+            cv2.rectangle(img, (cx + x, cy + y),
+                        (cx + x + w, cy + y + h), (255, 0, 0), 2)
+            cv2.circle(img, center, 4, (0, 0, 255), -1)
 
-    if is_car:
-        cv2.putText(img, "Carro", (25, 50),
-                    cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-    else:
-        cv2.putText(img, "Outro", (25, 50),
-                    cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+        if is_car:
+            cv2.putText(img, "Carro", (25, 50),
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+        else:
+            cv2.putText(img, "Outro", (25, 50),
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
 
-    cv2.imshow("Classica", img)
+        cv2.imshow("Classica", img)
 
     if cv2.waitKey(1) == 27:
         break
